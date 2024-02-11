1c352b16f6f194e49a6d7b7a369bb3037cfcf25b
sushant-wayal <sushantssr12345@gmail.com>
2024-02-12 03:11:36 +0530

    changed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://sushant-wayal.github.io/Exoape-Clone/',
+  base: '/Exoape-Clone/',
   plugins: [react()],
 })
