496d1ddf212231d7ea0c3078a47e94c2cb49b71e
sushant-wayal <sushantssr12345@gmail.com>
2024-02-12 02:46:50 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Exoape-Clone/'
+  base: '/Exoape-Clone/',
   plugins: [react()],
 })
